d7297f12fde9d1e06b56fac6891e593285dd8727
hashmaster02 <hashirshah222@gmail.com>
2025-11-12 20:14:39 -0600

    updated main.cpp

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -5,6 +5,7 @@
 #include "Orion/Texture.h"
 #include "Orion/Window.h"
 #include "Orion/Object.h"
+#include "Orion/Renderer.h"
 #include "Utils/stb_image.h"
 #include "glm/glm.hpp"
 #include <glm/gtc/matrix_transform.hpp>
@@ -17,8 +18,8 @@ int main() {
   orion_window->height = 720;
   orion_window->title = "Orion";
   orion_window->GLFWframebuffersizefun= Orion::framebuffer_size_callback;
-  Orion::create_window(orion_window);
 
+  Orion::create_window(orion_window);
   if(!orion_window->window) {
     printf("Failed to create window");
     return -1;
@@ -40,12 +41,18 @@ int main() {
 
   unsigned int shaderProgram = Orion::build_shader_program("./App/shaders/default_vertex.glsl", "./App/shaders/default_fragment.glsl");
   if (shaderProgram == 0) {
-    printf("ERROR: Couldn't build shader.");
+    printf("ERROR: Failed to build shader.");
     return -1;
   }
-  glUseProgram(shaderProgram);
+  Orion::use_shader(shaderProgram);
 
   unsigned int texture = Orion::generate_texture("./App/images/wall.jpg");
+  if (texture == 0) {
+    printf("ERROR: Failed to generate texture.");
+    return -1;
+  }
+  Orion::use_texture2d(texture);
+
   unsigned int VAO_cube = Orion::create_object(Orion::default_cube, Orion::default_cube_size);
 
   glm::mat4 projection    = glm::mat4(1.0f);
@@ -59,8 +66,6 @@ int main() {
     glClearColor(clearCol.x, clearCol.y, clearCol.z, clearCol.w);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBindTexture(GL_TEXTURE_2D, texture);
-
     glm::mat4 view_mat = glm::mat4(1.0f);
     float radius = 10.0f;
     float camX = static_cast<float>(sin(glfwGetTime()) * radius);
@@ -68,7 +73,7 @@ int main() {
     view_mat = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     Orion::setUniformMat4fv(shaderProgram, "view", view_mat);
     
-    glBindVertexArray(VAO_cube);
+    Orion::bind_vertex_array(VAO_cube);
     for (unsigned int i = 0; i < 10; i++)
     {
         // calculate the model matrix for each object and pass it to shader before drawing
@@ -78,15 +83,15 @@ int main() {
         model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
         Orion::setUniformMat4fv(shaderProgram, "model", model);
 
-        glDrawArrays(GL_TRIANGLES, 0, (unsigned int)(Orion::default_cube_size / sizeof(float)));
+        Orion::draw((unsigned int)(Orion::default_cube_size / sizeof(float)));
     }
 
     glfwSwapBuffers(orion_window->window);
     glfwPollEvents();
   }
 
-  glDeleteProgram(shaderProgram);
-  destroy_window(orion_window);
+  Orion::destroy_shader(shaderProgram);
+  Orion::destroy_window(orion_window);
 
   return 0;
 }
