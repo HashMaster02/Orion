2b22f97100c450304be65457175dffceaeff1c14
hashmaster02 <hashirshah222@gmail.com>
2025-11-14 09:40:06 -0600

    Removed Input.h and Input.cpp

--- Core/include/Orion/Input.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <GLFW/glfw3.h>
-
-namespace Orion {
-    void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-    void processInput(GLFWwindow *window);
-}
