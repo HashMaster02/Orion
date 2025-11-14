3b7e10f45676cd6b9f71968476751e493446fe5d
hashmaster02 <hashirshah222@gmail.com>
2025-11-14 10:00:04 -0600

    added function to calculate delta-time

--- Core/include/Orion/Renderer.h
+++ Core/include/Orion/Renderer.h
@@ -3,4 +3,5 @@
 namespace Orion {
     void bind_vertex_array(unsigned int VAO);
     void draw(unsigned int total_vertices);
+    float delta_time();
 }
--- Core/src/Orion/Renderer.cpp
+++ Core/src/Orion/Renderer.cpp
@@ -1,12 +1,23 @@
 #include "Orion/Renderer.h"
-#include "glad/glad.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 namespace Orion {
+    float lastFrame = 0.0f;
+
     void bind_vertex_array(unsigned int VAO) {
         glBindVertexArray(VAO);
     }
 
     void draw(unsigned int total_vertices) {
         glDrawArrays(GL_TRIANGLES, 0, total_vertices);
+    }
+
+    float delta_time() {
+        float currentFrame = glfwGetTime();
+        float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        return deltaTime;
     }
 }
