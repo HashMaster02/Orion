f1e7929bfe7fbffdf3b3c5a1d627a0b694a8ecf3
hashmaster02 <hashirshah222@gmail.com>
2025-11-14 09:41:57 -0600

    created Camera files to save global camera matrix parameters

---
+++ Core/include/Orion/Camera.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <glm/glm.hpp>
+
+namespace Orion {
+    extern glm::vec3 cameraPos;
+    extern glm::vec3 cameraFront;
+    extern glm::vec3 cameraUp;
+
+}
---
+++ Core/src/Orion/Camera.cpp
@@ -0,0 +1,8 @@
+#include "Orion/Camera.h"
+
+namespace Orion {
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+}
